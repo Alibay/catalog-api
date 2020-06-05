30e4aa87ec11ec119779d17de1b13243ac6bc1d3
ibraim ibragimov <ibraim.alibay@gmail.com>
2020-06-05 19:26:43 +0300

    added swagger

--- src/main/java/net/nomia/catalog/CatalogApplication.java
+++ src/main/java/net/nomia/catalog/CatalogApplication.java
@@ -2,7 +2,10 @@ package net.nomia.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class CatalogApplication {
 
@@ -10,4 +13,8 @@ public class CatalogApplication {
 		SpringApplication.run(CatalogApplication.class, args);
 	}
 
+	@GetMapping
+	public String api() {
+		return "redirect:/swagger-ui/index.html?url=/v3/api-docs";
+	}
 }
--- src/main/java/net/nomia/catalog/web/rest/CategoryController.java
+++ src/main/java/net/nomia/catalog/web/rest/CategoryController.java
@@ -1,13 +1,42 @@
 package net.nomia.catalog.web.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.nomia.catalog.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.noContent;
 
 @Tag(name = "Category", description = "API for categories")
 @RequestMapping("/api/categories")
 @RestController
 public class CategoryController {
 
+    private static final Logger log = LoggerFactory.getLogger(CategoryController.class);
 
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @Operation(description = "Create a category")
+    @PostMapping
+    public ResponseEntity createCategory() throws URISyntaxException {
+        return created(new URI("/api/categories/1")).build();
+    }
+
+    @Operation(description = "Delete a category")
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCategory(@PathVariable final Long id) {
+        log.trace("Delete a category, id: {}", id);
+        return noContent().build();
+    }
 }
